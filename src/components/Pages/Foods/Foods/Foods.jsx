ddd724bb4bc559a1a1021460876739d5f10c2b24
nrsabit <mailbox.sabit@gmail.com>
2023-05-27 20:29:00 +0600

    reviews loading from server

--- src/components/Pages/Foods/Foods/Foods.jsx
+++ src/components/Pages/Foods/Foods/Foods.jsx
@@ -6,10 +6,11 @@ import "react-tabs/style/react-tabs.css";
 import useMenu from "../../../../hooks/useMenu";
 import FoodTab from "../FoodTab/FoodTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Foods = () => {
-  const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks']
-  const {category} = useParams()
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
   const [menu] = useMenu();
   const drinksMenu = menu.filter((item) => item.category === "drinks");
   const saladMenu = menu.filter((item) => item.category === "salad");
@@ -19,12 +20,19 @@ const Foods = () => {
   const [tabIndex, setTabIndex] = useState(categories.indexOf(category));
   return (
     <div>
+      <Helmet>
+        <title>Bistro Boss | Foods</title>
+      </Helmet>
       <Cover
         bg={foodBg}
         title="Our Foods"
         subtitle="Would you like to try a Dish?"
       ></Cover>
-      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)} className="my-16">
+      <Tabs
+        selectedIndex={tabIndex}
+        onSelect={(index) => setTabIndex(index)}
+        className="my-16"
+      >
         <TabList>
           <Tab>Salads</Tab>
           <Tab>Pizzas</Tab>
@@ -33,11 +41,21 @@ const Foods = () => {
           <Tab>Drinks</Tab>
         </TabList>
 
-        <TabPanel><FoodTab items={saladMenu}></FoodTab> </TabPanel>
-        <TabPanel><FoodTab items={pizzaMenu}></FoodTab> </TabPanel>
-        <TabPanel><FoodTab items={soupMenu}></FoodTab> </TabPanel>
-        <TabPanel><FoodTab items={dessertMenu}></FoodTab> </TabPanel>
-        <TabPanel><FoodTab items={drinksMenu}></FoodTab> </TabPanel>
+        <TabPanel>
+          <FoodTab items={saladMenu}></FoodTab>
+        </TabPanel>
+        <TabPanel>
+          <FoodTab items={pizzaMenu}></FoodTab>
+        </TabPanel>
+        <TabPanel>
+          <FoodTab items={soupMenu}></FoodTab>
+        </TabPanel>
+        <TabPanel>
+          <FoodTab items={dessertMenu}></FoodTab>
+        </TabPanel>
+        <TabPanel>
+          <FoodTab items={drinksMenu}></FoodTab>
+        </TabPanel>
       </Tabs>
     </div>
   );
